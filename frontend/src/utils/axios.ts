5f517ec36135c6d6e2f6483ca133af075dd69265
Lucas Fernandes Ribeiro de Oliveira <136923413+lucas-fro@users.noreply.github.com>
2025-08-28 22:42:15 -0300

    adição do dotenv no front

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Cria uma instância do Axios com a base URL
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333", // usa .env se existir
+  baseURL: process.env.BASE_URL, // usa .env se existir
   headers: {
     "Content-Type": "application/json"
   }
